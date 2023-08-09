eea9e1c8c1c118e6c09471e8b38f4d4df0d91cea
arwaash01 <arwa8283@gmail.com>
2023-08-09 10:02:11 +0300

    Add college ti VM

--- LaboratoryProject/Models/RequestVM.cs
+++ LaboratoryProject/Models/RequestVM.cs
@@ -1,10 +1,14 @@
-﻿namespace LaboratoryProject.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LaboratoryProject.Models
 {
     public class RequestVM
     {
         public Request Request { get; set; }
 
         public List<DateTime> AvilableDates { get;  set; }
+
+        public SelectList CollagesSelectList { get; set; }
 
     }
 }
