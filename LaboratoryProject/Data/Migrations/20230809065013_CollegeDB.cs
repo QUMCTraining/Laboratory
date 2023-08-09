f3cba9b95e588302cde7a0dc2e1ef9957fe0fc67
RAlshoshan <renad4454@gmail.com>
2023-08-09 09:51:13 +0300

    Add College Model,Controller and view.

---
+++ LaboratoryProject/Data/Migrations/20230809065013_CollegeDB.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LaboratoryProject.Data.Migrations
+{
+    public partial class CollegeDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "College",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_College", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mangement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mangement", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "College");
+
+            migrationBuilder.DropTable(
+                name: "Mangement");
+        }
+    }
+}
---
+++ LaboratoryProject/Models/College.cs
@@ -0,0 +1,8 @@
+﻿namespace LaboratoryProject.Models
+{
+    public class College
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
